11a3268fc88e5f63da47c8377e4d109053f23288
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-12-28 07:37:12 -0500

    Define initial method structure

--- main.rb
+++ main.rb
@@ -1,19 +1,42 @@
+# frozen_string_literal: true
+
+# Class to play the classic paper and pencil game
 class TicTacToe
+  attr_accessor :positions, :player_x, :player_0
 
   def initialize
     @positions = (1..9).to_a
+    @player_x = { name: '', score: '', symbol: 'X'}
+    @player_0 = { name: '', score: '', symbol: '0'}
   end
 
-  def print_board
-    puts "X | 0 | 0"
-    puts "---------"
-    puts "0 | X | 0"
-    puts "---------"
-    puts "0 | 0 | X"
-    puts " Yay: #{@positions}"
+  def start
+    set_game
   end
 
+  private
+
+  def set_game
+    puts 'WELCOME TO TIC TAC TOE'
+    p player_x
+    p player_0
+    gets
+  end
+
+  def play_game
+  end
+
+  def play_round
+  end
+
+  def draw_board
+    puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
+    puts '--+---+---'
+    puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
+    puts '--+---+---'
+    puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
+  end
 end
 
 game = TicTacToe.new
-game.print_board
+game.start
