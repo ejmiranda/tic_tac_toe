f3669aae2eeceba8e984480ea7231c9b66c856c0
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-01-06 08:17:52 -0500

    Add turns

--- main.rb
+++ main.rb
@@ -1,42 +1,78 @@
 # frozen_string_literal: true
 
-# Class to play the classic paper and pencil game
+# The classic paper and pencil game
 class TicTacToe
-  attr_accessor :positions, :player_x, :player_0
+  attr_accessor :positions, :player_x, :player_o, :current_player
 
   def initialize
     @positions = (1..9).to_a
-    @player_x = { name: '', score: '', symbol: 'X'}
-    @player_0 = { name: '', score: '', symbol: '0'}
+    @player_x = { name: '', score: 0, symbol: 'X' }
+    @player_o = { name: '', score: 0, symbol: '0' }
+    @current_player = player_x
   end
 
   def start
     set_game
+    play_game
   end
 
   private
 
   def set_game
+    puts
+    puts '----------------------'
     puts 'WELCOME TO TIC TAC TOE'
-    p player_x
-    p player_0
-    gets
+    puts '----------------------'
+    puts
+    puts 'Who chooses X?'
+    # player_x[:name] = gets.chomp
+    player_x[:name] = 'Edgar'
+    puts player_x[:name]
+    puts
+    puts 'Who chooses 0?'
+    # player_o[:name] = gets.chomp
+    player_o[:name] = 'Lucas'
+    puts player_o[:name]
+    puts
+    puts '----------------------'
   end
 
   def play_game
+    loop do
+      draw_board
+      play_turn
+    end
   end
 
-  def play_round
+  def play_turn
+    puts "Pick a position #{current_player[:name]}"
+    pos = gets.chomp.to_i
+    if valid_position?(pos)
+      positions[pos - 1] = current_player[:symbol]
+    else
+      puts 'Sorry, that one is not valid.'
+      puts
+    end
+    self.current_player = current_player == player_o ? player_x : player_o
   end
+
+  def valid_position?(_pos)
+    true
+  end
+end
 
   def draw_board
+    puts
+    puts "#{current_player == player_x ? '>' : ' '} #{player_x[:name]}: #{player_x[:score]}"
+    puts "#{current_player == player_o ? '>' : ' '} #{player_o[:name]}: #{player_o[:score]}"
+    puts
     puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
     puts '--+---+---'
     puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
     puts '--+---+---'
     puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
+    puts
   end
-end
 
 game = TicTacToe.new
 game.start
