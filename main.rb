ad34da3d08130cab268377b0bb1b619f4d8c2dce
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-01-09 08:01:11 -0500

    Create preliminary loop structure for game and turn

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 # The classic paper and pencil game
 class TicTacToe
-  attr_accessor :positions, :player_x, :player_o, :current_player
+  attr_accessor :positions, :player_x, :player_o, :current_player, :winner
 
   def initialize
     @positions = (1..9).to_a
@@ -12,8 +12,15 @@ class TicTacToe
   end
 
   def start
-    set_game
-    play_game
+    loop do
+      set_game # Reset variables?
+      until winner?
+        draw_board
+        play_turn
+      end
+      end_game
+      break unless play_again?
+    end
   end
 
   private
@@ -41,11 +48,32 @@ class TicTacToe
     puts player_o[:name]
   end
 
-  def play_game
-    loop do
-      draw_board
-      play_turn
-    end
+  def winner?
+    calc_winner
+    # If winner is not set to nil later, this will always be true
+    winner.nil? ? false : true
+  end
+
+  def calc_winner
+    # Calculate and assign @winner
+    self.winner = player_x
+  end
+
+  def draw_board
+    puts
+    draw_score
+    puts
+    puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
+    puts '--+---+---'
+    puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
+    puts '--+---+---'
+    puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
+    puts
+  end
+
+  def draw_score
+    puts "#{current_player == player_x ? '>' : ' '} #{player_x[:name]}: #{player_x[:score]}"
+    puts "#{current_player == player_o ? '>' : ' '} #{player_o[:name]}: #{player_o[:score]}"
   end
 
   def play_turn
@@ -54,7 +82,7 @@ class TicTacToe
     if valid_position?(pos)
       positions[pos - 1] = current_player[:symbol]
     else
-      puts 'Sorry, that one is not valid.'
+      puts 'Sorry, that one is not valid.' # Needs to loop
       puts
     end
     self.current_player = current_player == player_o ? player_x : player_o
@@ -65,21 +93,26 @@ class TicTacToe
   end
 end
 
-def draw_board
-  puts
-  draw_score
-  puts
-  puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
-  puts '--+---+---'
-  puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
-  puts '--+---+---'
-  puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
-  puts
+def end_game
+  draw_board
+  puts "The winner is #{winner[:name]}!"
+  update_score
 end
 
-def draw_score
-  puts "#{current_player == player_x ? '>' : ' '} #{player_x[:name]}: #{player_x[:score]}"
-  puts "#{current_player == player_o ? '>' : ' '} #{player_o[:name]}: #{player_o[:score]}"
+def update_score
+  winner[:score] += 1
+end
+
+def play_again?
+  puts 'Do you want to play again?'
+  play_again = gets.chomp
+  # Add answer validation!
+  case play_again
+  when 'y'
+    true
+  else
+    false
+  end
 end
 
 game = TicTacToe.new
